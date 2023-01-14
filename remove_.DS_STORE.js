da10a4ab12b8da2ab727fe75ecbeb60e51cf3d2b
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2023-01-14 16:45:44 +0330

    I REMOVED THEM DS STORES

--- remove_.DS_STORE.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-const dr = ((path = './') => {
-    fs.readdirSync(path).forEach(e => {
-        if(fs.statSync(e).isDirectory()) dr(path + '/' + e)
-        else if(e === '.DS_STORE') fs.rmSync(path + '/' + e)
-    })
-})
